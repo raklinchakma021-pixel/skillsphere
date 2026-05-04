9131f484bc8497d304a51b579257e50bc7603ff3
Raklin Chakma <raklinchakma021@gmail.com>
2026-05-04 10:19:52 +0600

    domain changed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://skillsphere-ny5ju8fwc-raklinchakma021-pixels-projects.vercel.app"
+    baseURL: "https://skillsphere-kohl.vercel.app/"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
